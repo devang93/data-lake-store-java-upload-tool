f4f4f492035baeb7688d168d1f2e65fc303acbf8
Binu Varghese (PSP) <bvarghes@starbucks.com>
2017-07-11 18:45:09 +0530

    Adding new variables which user can use to specify the folder which needs to be packaged and the name of the zip file

--- gruntfile.js
+++ gruntfile.js
@@ -9,14 +9,18 @@ var config = {
 			// The proj file is not required, but using a value here since this would be required for archiva upload
 			sln: "InfoGridjavautility.proj",
 			version: "1.0.0",
-			//If the packagefolder is left blank by default it takes the Release folder as default
-			packagefolder:"\\target",
 			tests:{
 				 //Added test template sample
 			     path:[""],
 				 //Type will allow us to support multi-types, mstest,nunit etc.
 				 type: ""	
 			},
+			packagefolder:{
+				//If the packagefolder.path is left blank by default it takes the Release folder as default.
+				path 		:"\\releasejavautility",
+				// Name of the package can be specified by the user here.
+				packagename	:"igjavautility"
+			}
 		}
 		
 };
